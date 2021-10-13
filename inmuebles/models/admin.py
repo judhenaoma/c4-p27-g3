45751206ef0dfcee173a14b6a4e4131c37aca3a6
judhenaoma <geohazards.med@gmail.com>
2021-10-12 19:13:31 -0500

    Agregar el archivo requeriments.txt

---
+++ inmuebles/models/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .user import User
+from .inmueble import Inmueble
+
+
+
+admin.site.register(User)
+admin.site.register(Inmueble)
--- inmuebles/models/user.py
+++ inmuebles/models/user.py
@@ -47,3 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     USERNAME_FIELD = 'username'
     #REQUIRED_FIELDS = ['email', 'password']
+
+    def __str__(self):
+        return self.username
+    
