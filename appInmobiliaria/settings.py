48698f4d175a4ab5bf7f62444bb710d6ea9aaff5
judhenaoma <judhenaoma@unal.edu.com>
2021-10-23 10:15:40 -0500

    cambio settings

--- appInmobiliaria/settings.py
+++ appInmobiliaria/settings.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -166,3 +165,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
